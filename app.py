97ad4be9463a1bfe82665e943c10cd6e867dc7dc
Joe <19354285+Jlaurole@users.noreply.github.com>
2023-03-23 15:32:20 +0000

    new updates

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 2, "Data Analyst"
+    'location': 3, "Atlanta"
+    'salary': 4, "US. 250,000"
+  }
+  {
+    'id': 2,
+    'title': 2, "Software Engineer"
+    'location': 3, "San Francisco"
+    'salary': 4, "US. 450,000"
+  }
+  
+]
+
 @app.route("/")
-def hello_world():
-  return "Hello, Joe!"
+def hello_joe():
+  return render_template("home.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
